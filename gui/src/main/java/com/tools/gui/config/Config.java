cc7ae0c46700a6de3c5e7c863193717772048eda
ZHANGBO <1115746521@qq.com>
2019-11-06 10:16:30 +0800

    1.linux适配 ,优化服务部署

--- gui/src/main/java/com/tools/gui/config/Config.java
+++ gui/src/main/java/com/tools/gui/config/Config.java
@@ -6,26 +6,10 @@ public class Config {
     public static final boolean isModeSelector = true;
     public static final boolean isConfigSync = true;
 
-    /**
-     * 默认的Tomcat服务名
-     */
-    public static final String defaultTomcatServerName = "ThinkWinCRTomcat";
-    /**
-     * 默认的Tomcat端口
-     */
-    public static final String defaultTomcatPort = "8080";
 
     public static final String debugConfigPath;
 
     public static String downloadFilePath = null;
-    /**
-     * CR配置文件CR.properties
-     */
-    public static String CRConfigFileName = "CR.properties";
-    /**
-     * 工具部署时，记录的CR的配置文件列表  ConfigList.properties
-     */
-    public static String CRConfigListFileName = "ConfigList.properties";
     /**
      *  Jenkins配置文件JenkinsProperties.properties
      */
